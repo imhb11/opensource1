c8b4cc82477de65482051fb528cb7fec2b13064a
임혜빈 <hywu5464@naver.com>
2024-10-05 18:33:57 +0900

    mul,div add

--- assignment1.py
+++ assignment1.py
@@ -4,10 +4,19 @@ def add(a,b):
 def subtract(a,b):
     return a-b
 
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        return "0으로 나눌 수 없습니다."
+    return a / b
+
+
 m = int(input("1번째 숫자: "))
 k = input("수식: ")
 
-if (k != '+' and k != '-'):
+if (k != '+' and k != '-' and k != '/' and k != '*'):
     print("수식 없음")
     exit()
     
@@ -19,6 +28,11 @@ if(k == '+'):
     
 elif(k == '-'):
     print("결과:", subtract(m,n))
-
+    
+elif(k == '*'):
+    print("결과:", multiply(m,n))
+    
+elif(k == '/'):
+    print("결과:", divide(m,n))
 
     
